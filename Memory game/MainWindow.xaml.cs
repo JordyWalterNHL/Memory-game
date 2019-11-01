56cd82a98d7313fb6164d9a4beabad13a0f64d5c
Hylkinator <hylke.van.der.zee@student.nhlstenden.com>
2019-11-01 15:06:02 +0100

    added using

--- Memory game/MainWindow.xaml.cs
+++ Memory game/MainWindow.xaml.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
 using System.Windows.Threading;
 
 
